bc2d01f99aec4fab88821cc27894a49ca65c6faf
RicardoSalas17 <ricardosalas.1716@gmail.com>
2021-07-22 11:30:34 -0500

    agregando ty y catch apod

--- controllers/ApodController.js
+++ controllers/ApodController.js
@@ -2,17 +2,22 @@ const apodApi = require("../utils/apodAPI");
 
 module.exports = {
   getPhoto: async (req, res) => {
-    console.log("dentro")
+    try{
     const responses = await apodApi.getPhoto();
     res
       .status(200)
       .json({ message: "get another photo", daily_photo: responses.data });
+  }
+  catch {res.json({ message: 'error'})}
   },
   getDaily: async (req, res) => {
+try{
     const chosenDate = req.params.daily_photo;
     const response = await apodApi.getDaily("date", chosenDate);
     res
       .status(200)
       .json({ message: "get daily photo", daily_photo: response.data });
-  },
+  }
+      catch {res.json({ message: 'error'})}
+    }
 };
