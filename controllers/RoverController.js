9e2f8db37fc63ffcb91befb1b40556edda60608d
MontseOrtiz <montse_2908@hotmail.com>
2021-07-21 21:05:12 -0500

    agregando error

--- controllers/RoverController.js
+++ controllers/RoverController.js
@@ -2,21 +2,30 @@ const roverApi = require('../utils/roverAPI');
 
 module.exports = {
     getAll: async (req, res ) =>{
-        const roverName = req.params.rover_name;
-        const response = await roverApi.getAll(roverName, 'sol', 1000);
-        res.status(200).json({ message: 'Rover photos obtainer ', rover_photos: response.data });
+        try{
+            const roverName = req.params.rover_name;
+            const response = await roverApi.getAll(roverName, 'sol', 1000);
+            res.status(200).json({ message: 'Rover photos obtainer ', rover_photos: response.data });
+        }
+        catch {res.json({ message: 'error'})}
     },
 
     getRoverInfo: async (req, res ) =>{
-        const roverName = req.params.rover_name;
-        const response = await roverApi.getRoverInfo(roverName);
-        res.status(200).json({ message: 'Rover info obtainer ', rover_info: response.data });
+        try{
+            const roverName = req.params.rover_name;
+            const response = await roverApi.getRoverInfo(roverName);
+            res.status(200).json({ message: 'Rover info obtainer ', rover_info: response.data });
+        }
+        catch {res.json({ message: 'error'})}
     },
 
     getFilterPhotos: async (req, res ) =>{
-        const roverCamera = req.params.rover_camara;
-        const roverName = req.params.rover_name;
-        const response = await roverApi.getFilterPhotos(roverName, 'sol', 1000, 'camera', roverCamera );
-        res.status(200).json({ message: 'Rover filter by camera ', rover_filter: response.data });
+        try{
+            const roverCamera = req.params.rover_camara;
+            const roverName = req.params.rover_name;
+            const response = await roverApi.getFilterPhotos(roverName, 'sol', 1000, 'camera', roverCamera );
+            res.status(200).json({ message: 'Rover filter by camera ', rover_filter: response.data });
+        }
+        catch {res.json({ message: 'error'})}
     },
 };
